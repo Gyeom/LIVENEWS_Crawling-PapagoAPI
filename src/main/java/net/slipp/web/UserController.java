3af050057ea3caca0e373c08e6d223d4c7700c71
gyeom <koreatech93@naver.com>
2018-12-06 09:33:22 +0900

    login func update

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,29 +3,87 @@ package net.slipp.web;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import net.slipp.domain.User;
+import net.slipp.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	private List<User> users = new ArrayList<>();
-
-	@PostMapping("/create")
-	public String create(User user) {
-		System.out.println(user);
-		users.add(user);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		if(user==null) {
+			System.out.println("Login Faliure!");
+			return "redirect:/users/loginForm";
+		}
+		if(!password.equals(user.getPassword())) {
+			return "redirect:/users/loginForm";
+		}
+		System.out.println("Login Success");
+		session.setAttribute("user", user);
 		
 		
-		return "redirect:/list";
+		return "redirect:/";
+	}
+	
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	@GetMapping("/{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findById(id).get();
+		model.addAttribute("user", user);
+		return "/user/updateForm";
 	}
 	
 	@GetMapping("/list")
-	public String list(Model model) {
-		model.addAttribute("users", users);
+	public String list() {
+		return "/user/list";
+	}
+
+	@PostMapping("")
+	public String create(User user) {
 		
-		return "list";
+		userRepository.save(user);
+		
+		return "redirect:/users";
+	}
+	
+	@GetMapping("")
+	public String list(Model model) {
+		model.addAttribute("users", userRepository.findAll());
+		
+		return "/user/list";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findById(id).get();
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
 	}
 }
