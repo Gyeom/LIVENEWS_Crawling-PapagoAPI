a318b2acd77fb89b329e0dbe00c1a2c133c8975e
gyeom <koreatech93@naver.com>
2018-12-13 17:38:41 +0900

    migration

--- src/main/java/net/slipp/domain/NewsAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package net.slipp.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsAnswerRepository extends JpaRepository<NewsAnswer, Long> {
-
-}
--- src/main/java/net/slipp/domain/QuestionRepository.java
+++
@@ -1,7 +0,0 @@
-package net.slipp.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-}
